8f91d4054e6fcce5ae77711eddff0b455384e015
Sebastian Aegidius <66956640+SebastianAgi@users.noreply.github.com>
2022-07-22 17:50:54 +0100

    Update social_force.py

--- scripts/social_force.py
+++ scripts/social_force.py
@@ -3,15 +3,18 @@ import zed_interfaces.msg
 import geometry_msgs.msg
 import std_msgs.msg
 import rospy
+import time
 import math
 import message_filters
 import numpy as np
 from numpy import *
+import spot_driver.msg
 
 roll = pitch = yaw = 0.0
 
-pub = rospy.Publisher('/obj_too_close', zed_interfaces.msg.FloatList, queue_size=10)
+pub = rospy.Publisher('/obj_too_close', geometry_msgs.msg.Twist, queue_size=10)
 warn_info = zed_interfaces.msg.FloatList()
+velo = geometry_msgs.msg.Twist()
 flag = False
 avoid_left = False
 avoid_right = False
@@ -32,64 +35,6 @@ v_des = 1.6
 v_cur = 0
 
 
-def ellipse_center_dist(angle, a, b):
-    x = a/(math.sqrt((a**2 * math.tan(angle)**2 / b**2) + 1))
-    y = (a*math.tan(angle))/(math.sqrt((a**2 * math.tan(angle)**2 / b**2) + 1))
-    dist = math.sqrt(x**2 + y**2)
-    return dist, x, y
-
-def distance(x,y):
-    return math.sqrt(x**2 + y**2)
-
-def get_minvalue(inputlist):
-    #get the minimum value in the list
-    min_value = min(inputlist)
-    #return the index of minimum value 
-    min_index=inputlist.index(min_value)
-    return min_index
-
-def ccw(A_x,A_y,B_x,B_y,C_x,C_y):
-	return (C_y-A_y)*(B_x-A_x) > (B_y-A_y)*(C_x-A_x)
-
-def intersect(A_x,A_y,B_x,B_y,C_x,C_y,D_x,D_y):
-	return ccw(A_x,A_y,C_x,C_y,D_x,D_y) != ccw(B_x,B_y,C_x,C_y,D_x,D_y) and ccw(A_x,A_y,B_x,B_y,C_x,C_y) != ccw(A_x,A_y,B_x,B_y,D_x,D_y)
-
-def perp( a ) :
-    b = np.empty_like(a)
-    b[0] = -a[1]
-    b[1] = a[0]
-    return b
-
-# line segment a given by endpoints a1, a2
-# line segment b given by endpoints b1, b2
-# return 
-def seg_intersect(a1,a2, b1,b2) :
-    da = a2-a1
-    db = b2-b1
-    dp = a1-b1
-    dap = perp(da)
-    denom = dot( dap, db)
-    num = dot( dap, dp )
-    return (num / denom.astype(float))*db + b1
-
-def quaternion_to_euler_angle_vectorized2(w, x, y, z):
-    ysqr = y * y
-
-    t0 = +2.0 * (w * x + y * z)
-    t1 = +1.0 - 2.0 * (x * x + ysqr)
-    X = np.degrees(np.arctan2(t0, t1))
-
-    t2 = +2.0 * (w * y - z * x)
-
-    t2 = np.clip(t2, a_min=-1.0, a_max=1.0)
-    Y = np.degrees(np.arcsin(t2))
-
-    t3 = +2.0 * (w * z + x * y)
-    t4 = +1.0 - 2.0 * (ysqr + z * z)
-    Z = np.degrees(np.arctan2(t3, t4))
-
-    return X, Y, Z
-
 
 def normalized(a, axis=-1, order=2):
     l2 = np.atleast_1d(np.linalg.norm(a, order, axis))
@@ -101,16 +46,19 @@ def unit_vector(vector):
     return vector / np.linalg.norm(vector)
 
 def angle_between(v1, v2):
-    v1_u = unit_vector(v1)
-    v2_u = unit_vector(v2)
-    return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
+    v1_u = np.linalg.norm(v1)
+    v2_u = np.linalg.norm(v2)
+    v1a = np.array([v1[0,0],v1[1,0]])
+    v2a = np.array([v2[0,0],v2[1,0]])
+    angle = np.arccos(np.dot(v1a,v2a)/(v1_u*v2_u))
+    return angle
 
 def leftNormalVector(vec):
-    return np.array([[-vec[0,1]],
+    return np.array([[-vec[1,0]],
                      [vec[0,0]]])
 
-def desired_force(desired_direction, current_velo, RelaxationTime):
-    force = (desired_direction * 1,6 - current_velo) / RelaxationTime
+def desired_force(desired_direction, vmax, current_velo, RelaxationTime):
+    force = (desired_direction * vmax - current_velo) / RelaxationTime
     return force
 
 #spot is an np array (x,y vector), ped is obj_det_.objects
@@ -123,15 +71,16 @@ def social_force(spot, ped):
     
     for i in range(len(ped.objects)):
 
-        diff = np.array([[ped.objects[i].position[0]-spot[0]], 
-                         [ped.objects[i].position[1]-spot[1]]])
+        diff = np.array([[ped.objects[i].position[0]-spot[0,0]], 
+                         [ped.objects[i].position[1]-spot[1,0]]])
 
-        diffDirection = normalized(diff)
+        diffDirection = unit_vector(diff)
+        diffDirection = np.reshape(diffDirection, (2,1))
 
         velDiff = spot - np.array([[ped.objects[i].velocity[0]],
                                    [ped.objects[i].velocity[1]]])
 
-        interactionVector = lambda_ * velDiff + diffDirection
+        interactionVector = np.add(lambda_ * velDiff, diffDirection)
         interactionLength = np.linalg.norm(interactionVector)
         interactionDirection = interactionVector / interactionLength
 
@@ -159,10 +108,17 @@ def move(delta_time,socialforce, desiredforce, current_velo):
     return v
 
 
-def callback(velo_cam, obj_det, pose_cam):
-    global past_time, current_time
+def callback(spot, obj_det):
+    global past_time, current_time, velo
     
+    # rospy.loginfo(spot)
+    # rospy.loginfo(obj_det)
     RelaxationTime = 1
+    v_max = 1.6
+    goal_x = -4.922226052558839
+    goal_y = -13.30729253667893
+    spot_velo = np.array([[spot.velocity_of_body_in_odom.linear.x],
+                          [spot.velocity_of_body_in_odom.linear.y]])
 
     # ********************************
     # need current spot velocity
@@ -171,23 +127,38 @@ def callback(velo_cam, obj_det, pose_cam):
     # need current orientation of pedestrian x
     # ********************************
 
-    g_force = desired_force(desired_direction, spot_velo, RelaxationTime)
+    direction = np.array([[goal_x - spot.vision_tform_body.translation.x],
+                          [goal_y - spot.vision_tform_body.translation.y]])
+    desired_direction = unit_vector(direction)
+
+    g_force = desired_force(desired_direction, v_max, spot_velo, RelaxationTime)
     s_force = social_force(spot_velo, obj_det)
 
-    current_time = rospy.Time.now() if past_time != 0 else 0.0001
+    rospy.loginfo(g_force)
+    rospy.loginfo(s_force)    
+
+    current_time = rospy.Time.now()
+
+    if past_time == 0:
+        rospy.Time.set(current_time, 0, 500000)
+
     delta_time = current_time - past_time
     
     new_spot_velo = move(delta_time,s_force, g_force, spot_velo)
+    velo.linear.x = new_spot_velo[0,0]
+    velo.linear.y = new_spot_velo[1,0]
+
+    rospy.loginfo(velo)
+    pub.publish(velo)
     
     past_time = current_time
 
 
 def main():
-    velo_cam = message_filters.Subscriber('/zed2i/zed_node/self_velocity', geometry_msgs.msg.TwistStamped)
+    spot_kin = message_filters.Subscriber('/kinematic_state', spot_driver.msg.KinematicState)
     object_sub = message_filters.Subscriber('/zed2i/zed_node/obj_det/objects', zed_interfaces.msg.ObjectsStamped)
-    pose_cam = message_filters.Subscriber('/zed2i/zed_node/pose', geometry_msgs.msg.PoseStamped)
 
-    ts = message_filters.ApproximateTimeSynchronizer([velo_cam, object_sub, pose_cam],queue_size = 10, slop = 0.5)
+    ts = message_filters.ApproximateTimeSynchronizer([spot_kin, object_sub],queue_size = 100, slop = 0.1)
     ts.registerCallback(callback)
 
     # rospy.Subscriber('/zed2/zed_node/obj_det/objects', zed_interfaces.msg.ObjectsStamped, callback)
