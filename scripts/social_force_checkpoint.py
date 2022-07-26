0febf1ea28cc2c508c8269de4db7d257d957bfd9
Sebastian Aegidius <66956640+SebastianAgi@users.noreply.github.com>
2022-07-26 17:42:26 +0100

    Update social_force_checkpoint.py

--- scripts/social_force_checkpoint.py
+++ scripts/social_force_checkpoint.py
@@ -16,10 +16,10 @@ import spot_driver.msg
 pub = rospy.Publisher('/obj_too_close', zed_interfaces.msg.FloatList, queue_size=10)
 velo = zed_interfaces.msg.FloatList()
 flag = False
-resume_checkpoint = 0
+resume_checkpoint = 1
 past_time = 0
 current_time = 0
-current_checkpoint = 0
+current_checkpoint = 1
 file_list = []
 
 path = '/home/prl-orin/ros_ws/src' #Directory to load Pose file from
@@ -45,12 +45,22 @@ for i in range(9):
     Poses.append(pose)
 
 def check_next_pose(x, y):
-    current_checkpoint = 0 
-    ##########################################
-    # Add check for what checkopoint to follow
-    # add the pose file for the pose list
-    # finish model
-    ##########################################
+    global resume_checkpoint, Poses, current_checkpoint
+
+    vec = np.array([[x - Poses[current_checkpoint].position.x],
+                    [y - Poses[current_checkpoint].position.y]])
+
+    if  np.linalg.norm(vec) < 1:
+        current_checkpoint += 1
+        if current_checkpoint == 4:
+        # goal reached
+            current_checkpoint = 3
+            resume_checkpoint = 1
+
+    # strink = '\nx,y: {},{}\next checkpoint ({})(pose {}): \n{}\nnorm(vec): {}'.format(x,y,current_checkpoint,current_checkpoint+1,Poses[current_checkpoint].position,np.linalg.norm(vec))
+    # rospy.loginfo(strink)
+
+    return current_checkpoint, resume_checkpoint
 
 def unit_vector(vector):
     """ Returns the unit vector of the vector.  """
@@ -62,16 +72,17 @@ def desired_force(desired_direction, vmax, current_velo, RelaxationTime):
 
 #spot is an np array (x,y vector), ped is obj_det_.objects
 def social_force(spot, ped):
+    forces = 0
     force = 0
-    lambda_ = 1.0
+    lambda_ = 0.75
     A = 10 #5.5
-    B = -3 #-2
-    C = 4 #1.7
+    B = -10.2 #-2
+    C = 10 #1.7
     
     for i in range(len(ped.objects)):
 
-        diff = np.array([[ped.objects[i].position[0]-spot[0,0]], 
-                         [ped.objects[i].position[1]-spot[1,0]]])
+        diff = np.array([[ped.objects[i].position[0]], 
+                         [ped.objects[i].position[1]]])
 
         diffDirection = unit_vector(diff)
         diffDirection = np.reshape(diffDirection, (2,1))
@@ -83,16 +94,18 @@ def social_force(spot, ped):
             ped_velo = np.array([[ped.objects[i].velocity[0]],
                                  [ped.objects[i].velocity[1]]])
 
-        velDiff = spot - ped_velo
+        # velDiff = spot - ped_velo
 
-        interactionVector = np.add(lambda_ * velDiff, diff)
+        # interactionVector = np.add(lambda_ * velDiff, diff)
         
-        repulsive_force = A*math.exp(B*np.linalg.norm(interactionVector) + C) # Ae^(Bx + C)
+        repulsive_force = A*math.exp(B*np.linalg.norm(diff) + C) # Ae^(Bx + C)
         
         force = np.array([[diffDirection[0,0]*repulsive_force],
                           [diffDirection[1,0]*repulsive_force]])
+        
+        forces += force
 
-    return force
+    return forces
 
 def move(delta_time,socialforce, desiredforce, current_velo):
 
@@ -103,25 +116,27 @@ def move(delta_time,socialforce, desiredforce, current_velo):
 
 
 def callback(spot, obj_det):
-    global past_time, current_time, velo, flag
+    global past_time, current_time, velo, flag, current_checkpoint, resume_checkpoint
     RelaxationTime = 0.5
     v_max = 0.5
-    v_rotate = 0.5
-    goal_x = 4#-4.902107704062697
-    goal_y = 0#-18.595383951711007
-    threshold = 0.45
+    goal_x = 4.902107704062697
+    goal_y = 0.378
+    threshold = 0.01
 
     if flag == False:
         past_time = rospy.Time.now()
         flag = True
 
-    spot_velo = np.array([[spot.velocity_of_body_in_odom.linear.x],
-                          [spot.velocity_of_body_in_odom.linear.y]])
-    # spot_velo = np.array([[0.3],
-    #                       [-0.95]])
+    # spot_velo = np.array([[spot.velocity_of_body_in_odom.linear.x],
+    #                       [spot.velocity_of_body_in_odom.linear.y]])
+    spot_velo = np.array([[1],
+                          [0]])
 
-    direction = np.array([[goal_x - spot.vision_tform_body.translation.x],
-                          [goal_y - spot.vision_tform_body.translation.y]])
+    spot_position = np.array([[spot.vision_tform_body.translation.x],
+                              [spot.vision_tform_body.translation.y]])
+
+    direction = np.array([[goal_x - spot_position[0,0]],
+                          [goal_y - spot_position[1,0]]])
 
     desired_direction = unit_vector(direction)
 
@@ -129,28 +144,35 @@ def callback(spot, obj_det):
 
     s_force = social_force(spot_velo, obj_det)
     
-    if np.linalg.norm(s_force) < threshold:
-        resume_checkpoint = 1
-    else:
+    current_checkpoint, resume_checkpoint = check_next_pose(spot_position[0,0],spot_position[1,0])
+
+    if np.linalg.norm(s_force) > threshold:
         resume_checkpoint = 0
+    else:
+        resume_checkpoint = 1
 
     current_time = rospy.Time.now()
     delta_time = float(rospy.Time.__str__(rospy.Time.__sub__(current_time,past_time)))/10**9
     
     new_spot_velo = move(delta_time,s_force, g_force, spot_velo)
 
-    forces = '\nrepulsive:  {}    attractive: {}          velo: {}\n           {}               {}               {}\n\nrepul norm: {},    attra norm: {},     velo norm: {}'.format(round(s_force[0,0],3),
-                                                                                                                                                                                    round(g_force[0,0],3),
-                                                                                                                                                                                    round(new_spot_velo[0,0],3),
-                                                                                                                                                                                    round(s_force[1,0],3),
-                                                                                                                                                                                    round(g_force[1,0],3),
-                                                                                                                                                                                    round(new_spot_velo[1,0],3),
-                                                                                                                                                                                    round(np.linalg.norm(s_force),3),
-                                                                                                                                                                                    round(np.linalg.norm(g_force),3),
-                                                                                                                                                                                    round(np.linalg.norm(new_spot_velo)))
-    rospy.loginfo(forces)
+    velo = [round(new_spot_velo[0,0],3), round(new_spot_velo[1,0],3), resume_checkpoint, current_checkpoint+1]
 
-    velo = [new_spot_velo[0,0], new_spot_velo[1,0], resume_checkpoint, current_checkpoint]
+    if len(obj_det.objects) != 0:
+        forces = '\nrepulsive:  {}    attractive: {}          velo: {}\n           {}               {}               {}\n\nrepul norm: {},    attra norm: {},     velo norm: {}\nvelo: {}'.format(round(s_force[0,0],3),
+                                                                                                                                                                                        round(g_force[0,0],3),
+                                                                                                                                                                                        round(new_spot_velo[0,0],3),
+                                                                                                                                                                                        round(s_force[1,0],3),
+                                                                                                                                                                                        round(g_force[1,0],3),
+                                                                                                                                                                                        round(new_spot_velo[1,0],3),
+                                                                                                                                                                                        round(np.linalg.norm(s_force),3),
+                                                                                                                                                                                        round(np.linalg.norm(g_force),3),
+                                                                                                                                                                                        round(np.linalg.norm(new_spot_velo)),
+                                                                                                                                                                                        velo)
+        rospy.loginfo(forces)
+
+    # strink = '\n{}\n{}'.format(np.linalg.norm(s_force),velo)
+    # rospy.loginfo(velo)
 
     pub.publish(velo)
     
