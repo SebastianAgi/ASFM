8af1239af8600400e337821f65c95b8361a79786
Sebastian Aegidius <66956640+SebastianAgi@users.noreply.github.com>
2022-08-10 12:40:23 +0100

    Update social_force_w_follow.py

--- scripts/social_force_checkpoint/social_force_w_follow.py
+++ scripts/social_force_checkpoint/social_force_w_follow.py
@@ -19,13 +19,18 @@ velo = zed_interfaces.msg.FloatList()
 flag = False
 follow_flag = False
 people_nearby = False
+leader_pos = np.array([[0],[0]])
+leader_velo = np.array([[0],[0]])
 resume_checkpoint = 1
 past_time = 0
 current_time = 0
-current_checkpoint = 2
+current_checkpoint = 1
 file_list = []
 spot_velo = np.array([[0],[0]])
+rot_vel = 0
 rotation = 0
+leader = None
+leader_too_close = False
 
 path = '/home/prl-orin/ros_ws/src' #Directory to load Pose file from
 load_file_name = "Pose_load.txt"
@@ -57,8 +62,8 @@ def check_next_pose(x, y):
 
     if  np.linalg.norm(vec) < 1:
         current_checkpoint += 1
-        if current_checkpoint == 4: # goal2 reached
-            current_checkpoint = 3
+        if current_checkpoint == 3: # goal reached
+            current_checkpoint = 2
             resume_checkpoint = 1
 
     return current_checkpoint, resume_checkpoint
@@ -74,43 +79,67 @@ def angle_between(vec1,vec2):
 
     return ang_between
 
-def odom_to_baselink(spot, odom_dir):
-    global rotation
+def c_rotation(vector, angle):
+    rotated_vector = np.array([[np.cos(angle)*vector[0,0] - np.sin(angle)*vector[1,0]],
+                               [np.sin(angle)*vector[0,0] + np.cos(angle)*vector[1,0]]])
 
+    return rotated_vector
+
+def cc_rotation(vector, angle):
+    rotated_vector = np.array([[ np.cos(angle)*vector[0,0] + np.sin(angle)*vector[1,0]],
+                               [-np.sin(angle)*vector[0,0] + np.cos(angle)*vector[1,0]]])
+
+    return rotated_vector
+
+def odom_to_baselink(spot, odom_dir):
+    global rot_vel
+    # spot rotation in global odom
     ang = math.atan2(2 * spot.rotation.z * spot.rotation.w + 2 * spot.rotation.x * spot.rotation.y, 1 - 2 * spot.rotation.y**2 - 2 * spot.rotation.z**2)
 
     # baselink desired direction to checkpoint in odometry
-    bl_d_d = np.array([[ np.cos(ang)*odom_dir[0,0] + np.sin(ang)*odom_dir[1,0]],
-                       [-np.sin(ang)*odom_dir[0,0] + np.cos(ang)*odom_dir[1,0]]])
-    
+    bl_d_d = cc_rotation(odom_dir, ang)
+
     spot_dir = np.array([[1],
                          [0]])
-
+    #angle between origin pose and goal point
     ang_between = angle_between(spot_dir,bl_d_d)
-
+    # An inacurate angle to show determine if clockwise or counter clockwise rotation is needed 
     angle_to = np.arctan2(spot_dir[0,0]*bl_d_d[1,0] - bl_d_d[0,0]*spot_dir[1,0], spot_dir[0,0]*spot_dir[1,0] + bl_d_d[0,0]*bl_d_d[1,0])
 
     degree = ang_between*180/np.pi
 
-    if angle_to > 0:
-        if degree > 45.0:
-            rotation = 1.5
-        elif int(degree) in range(5, 45):
-            rotation = 0.5
+    if follow_flag == False:
+        if angle_to > 0:
+            if degree > 45.0:
+                rot_vel = 1.5
+            elif int(degree) in range(5, 45):
+                rot_vel = 0.5
+            else:
+                rot_vel = 0.05
         else:
-            rotation = 0.05
+            if degree > 45.0:
+                rot_vel = -1.5
+            elif int(degree) in range(5, 45):
+                rot_vel = -0.5
+            else:
+                rot_vel = -0.05
+    
     else:
-        if degree > 45.0:
-            rotation = -1.5
-        elif int(degree) in range(5, 45):
-            rotation = -0.5
+        if angle_to > 0:
+            if degree > 45.0:
+                rot_vel = 0.5
+            else:
+                rot_vel = 0.05
         else:
-            rotation = -0.05
+            if degree > 45.0:
+                rot_vel = -0.5
+            else:
+                rot_vel = -0.05
 
-    # srtang = '\nrotation: {}\nangle_to:\n{}\nang_between:\n{}'.format(rotation,angle_to,ang_between)
+    # srtang = '\nrot_vel: {}\nangle_to:\n{}\nang_between:\n{}'.format(rot_vel,angle_to,ang_between)
     # rospy.loginfo(srtang)
 
-    return bl_d_d, rotation
+    return bl_d_d, rot_vel, ang
 
 def desired_force(desired_direction, vmax, current_velo, RelaxationTime):
 
@@ -119,10 +148,15 @@ def desired_force(desired_direction, vmax, current_velo, RelaxationTime):
 
 #spot is an np array (x,y vector), ped is obj_det_.objects
 def social_force(spot, ped):
-    global spot_velo, s_force, follow_flag, people_nearby
+    global spot_velo, s_force, follow_flag, people_nearby, leader_pos, distances, leader, leader_velo, leader_too_close
     forces = 0
     force = 0
+    threshold = 0.1
     lambda_ = 0.75
+    ids = []
+    dists = []
+    leader_too_close = False
+
     x = 0
     A = 0.1 # 10 #5.5
     B = -2 # -10.2 #-2
@@ -131,17 +165,23 @@ def social_force(spot, ped):
     A2 = 6.0
     B2 = 3.0
     dt = 0.5
-    
-    distances = np.empty([1,2])
 
     for i in range(len(ped.objects)):
-        distances[i,0] = ped.objects[i].label_id
-        distances[i,1] = np.linalg.norm(np.array([[ped.objects[i].position[0]], 
-                                                  [ped.objects[i].position[1]]]))
+        ids.append(ped.objects[i].label_id)
+        dists.append(sqrt( ped.objects[i].position[0]**2 + ped.objects[i].position[1]**2 ))
+
+    distances = np.array([ids,dists]).T
+
+    if leader not in distances:
+        leader = None
+    elif leader in distances:
+        n,m = np.where(distances == leader)
+        if distances[n,1] > 4.0:
+            leader = None       
     
     for i in range(len(ped.objects)):
 
-        diff = np.array([[ped.objects[i].position[0]], 
+        diff = np.array([[ped.objects[i].position[0]+0.25], 
                          [ped.objects[i].position[1]]])
         diffDirection = unit_vector(diff)
         diffDirection = np.reshape(diffDirection, (2,1))
@@ -183,27 +223,38 @@ def social_force(spot, ped):
             force = np.array([[diffDirection[0,0]*repulsive_force],
                               [diffDirection[1,0]*repulsive_force]])
 
+            if ped.objects[i].label_id == leader:
+                leader_pos = diff
+                leader_velo = np.array([[0],[0]])
+
+                if np.linalg.norm(diff) < 0.5:
+                    leader_too_close = True
+
         # If pedestrian is moving apply an elliptical force model
         else:
             ped_velo = np.array([[ped.objects[i].velocity[0]],
                                  [ped.objects[i].velocity[1]]])
 
-            ped_spot_ang = angle_between(unit_vector(ped_velo),unit_vector(spot_velo))
+            ped_spot_ang = angle_between(unit_vector(ped_velo),unit_vector(np.array([[1],[0]])))
+            # rospy.loginfo(ped_spot_ang*180/np.pi)
 
             x_ind,y_ind = np.unravel_index(np.argmin(distances, axis = None),distances.shape)
 
-            if ped_spot_ang < 30 and distances[x_ind,0] == ped.objects[i].label_id and people_nearby == True
-
-                if follow_flag == False:
+            if ped_spot_ang*180/np.pi < 30 and distances[x_ind,0] == ped.objects[i].label_id and distances[x_ind,1] < 4.0 and people_nearby == True:
+                strong = 'there is a leader now\nped_velo:{}\nangle:{}'.format(ped_velo,ped_spot_ang*180/np.pi)
+                # rospy.loginfo(strong)
+                
+                if leader != None and != ped.objects[i].label_id:
+                    break
+                
+                leader = ped.objects[i].label_id
                 follow_flag = True
-
-                # goal_x = diff[0,0]
-                # goal_y = diff[1,0]
-
-                # An approach here would be to just have the follow flag use the old follow method, 
-                # but restrict the velocity to the velo of the person followed
-
-
+                leader_pos = diff
+                leader_velo = ped_velo
+                force = 0
+                if np.linalg.norm(diff) < 1.5:
+                    leader_too_close = True
+                rospy.loginfo('\nleader_velo: '+str(leader_velo))
             else:
                 
                 ped_velo_t = np.linalg.norm(ped_velo)
@@ -223,53 +274,76 @@ def social_force(spot, ped):
                 repulsive_force = initial * second * third_2
 
                 rep_force = np.array([[repulsive_force[0,0]],
-                                    [2*repulsive_force[1,0]]])
+                                      [repulsive_force[1,0]]])
 
-                rospy.loginfo(rep_force)
+                # rospy.loginfo(rep_force)
 
-                force = rep_force
-        
+                force = repulsive_force
+                #############################
+                # CHECK IF IT IS RIGHT TYPE # 
+                #############################
+
         forces += force
+
+        strung = '\nforce: {}'.format(forces)
+        rospy.loginfo(strung)
     
-    if forces > threshold:
-        people_nearby = True
-    else:
-        people_nearby = False
+        if np.linalg.norm(forces) > -0.5:
+            people_nearby = True
+        else:
+            people_nearby = False
 
     return forces
 
-def move(delta_time,socialforce, desiredforce, current_velo, allowed_v):
+def move(delta_time,socialforce, desiredforce, current_velo, allowed_v, follow_flag):
+    global leader_velo
+    multiplier = 0
 
-    a = np.add(-socialforce,desiredforce)
-    # a = desiredforce
+    if follow_flag == True:
+        a = 10*desiredforce
+    else:
+        a = np.add(-socialforce,desiredforce)
+
     v = current_velo + delta_time * a
     
     max_velo = allowed_v
-    if v[0,0] > max_velo:
-        v[0,0] = max_velo
-    elif v[0,0] < 0:
-        v[0,0] = 0
-    if v[1,0] > max_velo:
-        v[1,0] = max_velo
-    elif v[1,0] < -max_velo:
-        v[1,0] = -max_velo
+    rospy.loginfo('\nallowed_velo: '+str(allowed_v))
+    
+    if leader != None:
+        max_velo = np.linalg.norm(leader_velo)
 
-    return v
+    length = sqrt(v[0,0]**2 + v[1,0]**2)
+
+    if length > max_velo and max_velo != 0:
+        multiplier = max_velo/length
+    else:
+        multiplier = 1
+    
+    clampedv = np.array([[v[0,0]*multiplier],[v[1,0]*multiplier]])
+    
+    if clampedv[0,0] < -0.01:
+        clampedv[0,0] = 0
+
+    if leader_too_close == True:
+        clampedv = np.zeros([2,1])
+
+    rospy.loginfo('\nspot_velo:' + str(clampedv))
+    return clampedv
 
 def duration(spot_pose,ped, cc):
-    global Poses
+    global Poses, distances
     velocity = 1
     ped_dist = np.empty((0), float)
 
-    for i in range(len(ped.objects)):
-        temp = np.linalg.norm(np.array([[ped.objects[i].position[0]],[ped.objects[i].position[1]]]))
-        temp_arr = np.array([temp])
-        ped_dist = np.append(ped_dist, temp_arr, axis=0)
+    # for i in range(len(ped.objects)):
+    #     temp = np.linalg.norm(np.array([[ped.objects[i].position[0]],[ped.objects[i].position[1]]]))
+    #     temp_arr = np.array([temp])
+    #     ped_dist = np.append(ped_dist, temp_arr, axis=0)
 
     if len(ped.objects) != 0:
-        if ped_dist.min() > 3 and ped_dist.min() < 4:
+        if distances[:,1].min() > 3 and distances[:,1].min() < 4:
             velocity = 0.85
-        elif ped_dist.min() < 3:
+        elif distances[:,1].min() < 3:
             velocity = 0.7
         else:
             velocity = 1
@@ -283,13 +357,12 @@ def duration(spot_pose,ped, cc):
         
 
 def callback(spot, obj_det):
-    global past_time, current_time, spot_velo, velo, flag, current_checkpoint, resume_checkpoint, Poses, rotation
-    global goal_x, goal_y
+    global past_time, current_time, spot_velo, velo, flag, current_checkpoint, resume_checkpoint, Poses, rot_vel
+    global follow_flag, leader_pos, rotation, leader
     allowed_velo = 1
+    angle_to_goal = 0
     RelaxationTime = 0.5
     v_max = 1
-    goal_x = Poses[current_checkpoint].position.x
-    goal_y = Poses[current_checkpoint].position.y
 
     if flag == False:
         past_time = rospy.Time.now()
@@ -302,12 +375,27 @@ def callback(spot, obj_det):
     spot_position = np.array([[spot.vision_tform_body.translation.x],
                               [spot.vision_tform_body.translation.y]])
 
+    if leader == None:
+        follow_flag = False
+
+    if follow_flag == True:
+        odom_leader_pos = c_rotation(leader_pos,rotation)
+        goal_x = spot_position[0,0] + odom_leader_pos[0,0]
+        goal_y = spot_position[1,0] + odom_leader_pos[1,0]
+
+        stringu = '\ngoal_x: {}\ngoal_y :{}\nspot_coord: {}\n'.format(goal_x,goal_y, spot_position)
+        rospy.loginfo(stringu)
+    
+    else:
+        goal_x = Poses[current_checkpoint].position.x
+        goal_y = Poses[current_checkpoint].position.y
+
     direction = np.array([[goal_x - spot_position[0,0]],
                           [goal_y - spot_position[1,0]]])
 
     odom_desired_direction = unit_vector(direction)
 
-    desired_direction, rotation = odom_to_baselink(spot.vision_tform_body, odom_desired_direction)
+    desired_direction, rot_vel, rotation = odom_to_baselink(spot.vision_tform_body, odom_desired_direction)
     
     g_force = desired_force(desired_direction, v_max, spot_velo, RelaxationTime)
     
@@ -324,9 +412,9 @@ def callback(spot, obj_det):
     delta_time = float(rospy.Time.__str__(rospy.Time.__sub__(current_time,past_time)))/10**9
     past_time = current_time
     
-    new_spot_velo = move(delta_time,s_force, g_force, spot_velo, allowed_velo)
+    new_spot_velo = move(delta_time,s_force, g_force, spot_velo, allowed_velo, follow_flag)
 
-    velo = [new_spot_velo[0,0], new_spot_velo[1,0], 0, current_checkpoint+1, time, rotation]
+    velo = [new_spot_velo[0,0], new_spot_velo[1,0], 0, current_checkpoint+1, time, rot_vel]
     spot_velo = new_spot_velo
 
     # if len(obj_det.objects) != 0:
@@ -343,7 +431,7 @@ def callback(spot, obj_det):
     #     rospy.loginfo(forces)
 
     # strink = '\n{}\n{}'.format(np.linalg.norm(s_force),velo)
-    # rospy.loginfo(velo)
+    rospy.loginfo(velo)
 
     pub.publish(velo)
     
