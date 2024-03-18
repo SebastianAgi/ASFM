47dcd1c0df4f4df137b1ce33a9967da0f9c0062a
Sebastian Aegidius <66956640+SebastianAgi@users.noreply.github.com>
2024-03-18 10:51:45 +0000

    Update ASFM_2.py

--- scripts/social_force_checkpoint/ASFM_2.py
+++ scripts/social_force_checkpoint/ASFM_2.py
@@ -308,9 +308,6 @@ def social_force(spot, ped):
                 if np.linalg.norm(diff) < 1.5:
                     leader_too_close = True
 
-                # rospy.loginfo(2_2)
-                # rospy.loginfo('\nleader_velo: '+str(leader_velo))
-
             else:
 
                 #############################################
